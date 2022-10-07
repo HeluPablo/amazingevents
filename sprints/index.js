77f87f20440ce5ee69316653cbd8e45fd2580f59
HeluPablo <pablo_andaas@hotmail.com>
2022-10-07 02:53:59 -0300

    Tarea 2

---
+++ sprints/index.js
@@ -0,0 +1,20 @@
+let contenedorHTML = document.getElementById("tarjetas")
+
+
+function cardPrinter(contenedor2) {
+    for (let event of events) {
+        contenedor2.innerHTML +=
+            `
+                 <div class="card align-items-center pb-2" style="width: 18rem;">
+                 <img src= "${event.image}" class="card-img-top" alt="${event.name}">
+                 <div class="card-body">
+                 <h5 class="card-title">${event.category}</h5>
+                 <p class="card-text">${event.description}</p>
+                  </div>
+                <p>Price: ${event.price}</p>
+                <a href="../details.html" class="btn btn-primary">Show me more</a>
+                </div>
+         `
+    }
+}
+cardPrinter(contenedorHTML)
