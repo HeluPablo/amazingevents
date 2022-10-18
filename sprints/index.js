a91f35a631e62f65df76b3a5f25ef6ed74eb654c
HeluPablo <100731170+HeluPablo@users.noreply.github.com>
2022-10-18 15:10:37 -0300

    Add files via upload
    Acá comiteando (?

--- sprints/index.js
+++ sprints/index.js
@@ -12,9 +12,27 @@ function cardPrinter(contenedor2) {
                  <p class="card-text">${event.description}</p>
                   </div>
                 <p>Price: ${event.price}</p>
-                <a href="../details.html" class="btn btn-primary">Show me more</a>
+                <a href="./details.html" class="btn btn-primary">Show me more</a>
                 </div>
          `
     }
 }
 cardPrinter(contenedorHTML)
+
+events.forEach(function (element) {
+    document.getElementById("checkbox-cont")
+    element.category
+    let contenedor = document.getElementById("checkbox-cont")
+    checkboxPrinter(contenedor)
+})
+
+function checkboxPrinter(contenedor) {
+    for (let element of categoria) {
+        contenedor.innerHTML +=
+            ` <div class="form-check form-check-inline">
+            <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
+            <label class="form-check-label" for="inlineCheckbox1">Category</label>
+        </div>
+         `
+    }
+}
